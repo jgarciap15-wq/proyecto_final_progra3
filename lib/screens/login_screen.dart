c361113771077da20a92e396b1566759086b22d7
Ivan <jgarciap15@miumg.edu.gt>
2026-05-26 21:44:02 -0600

    V1.3

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -23,7 +23,7 @@ class _LoginScreenState extends State<LoginScreen> {
     String usuario = usuarioController.text;
     String password = passwordController.text;
 
-    if (usuario == "jgarcia" && password == "1234") {
+    if (usuario == "jgarcia" && password == "12345") {
 
       Navigator.push(
         context,
